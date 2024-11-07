cbf29490cbffc096c1e3fa39f73bf7f97da30b6a
Aleksis Daugats <98739151+JustMangoo@users.noreply.github.com>
2024-11-07 23:31:55 +0100

    main screen animation

---
+++ scripts/changeLayout.js
@@ -0,0 +1,32 @@
+let inactivityTimer;
+
+function changeLayoutToInactive() {
+  var page = document.getElementById("index-page");
+
+  page.classList.remove("active");
+}
+
+function changeLayoutToActive() {
+  var page = document.getElementById("index-page");
+
+  page.classList.add("active");
+}
+
+function resetInactivityTimer() {
+  clearTimeout(inactivityTimer);
+  inactivityTimer = setTimeout(() => {
+    changeLayoutToInactive();
+  }, 2000);
+}
+
+document
+  .getElementById("exploreBtn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    changeLayoutToActive();
+    resetInactivityTimer();
+  })
+
+  [("onclick", "onmousemove", "onkeypress")].forEach((eventType) => {
+    document.addEventListener(eventType, resetInactivityTimer);
+  });
