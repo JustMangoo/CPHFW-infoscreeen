3d925ed089f8054f330330bf0668446751effce0
Aleksis Daugats <98739151+JustMangoo@users.noreply.github.com>
2024-11-15 02:51:01 +0100

    updated 404 page

--- scripts/inactivityTimer.js
+++ scripts/inactivityTimer.js
@@ -1,5 +1,5 @@
 let inactivityTimeout;
-const inactivityLimit = 15000; // Inactivity time limit in milliseconds
+const inactivityLimit = 150000; // Inactivity time limit in milliseconds
 
 function resetInactivityTimer() {
   clearTimeout(inactivityTimeout);
