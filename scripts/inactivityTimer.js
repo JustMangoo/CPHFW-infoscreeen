c74bccf98674fa63b03e1c32d81f0fc26b671a80
Aleksis Daugats <98739151+JustMangoo@users.noreply.github.com>
2024-11-15 02:31:09 +0100

    added inactivity timer

---
+++ scripts/inactivityTimer.js
@@ -0,0 +1,17 @@
+let inactivityTimeout;
+const inactivityLimit = 10000; // Inactivity time limit in milliseconds
+
+function resetInactivityTimer() {
+  clearTimeout(inactivityTimeout);
+  inactivityTimeout = setTimeout(() => {
+    window.location.href = "index.html";
+  }, inactivityLimit);
+}
+
+window.addEventListener("mousemove", resetInactivityTimer);
+window.addEventListener("keypress", resetInactivityTimer);
+window.addEventListener("scroll", resetInactivityTimer);
+window.addEventListener("click", resetInactivityTimer);
+window.addEventListener("touchstart", resetInactivityTimer);
+
+resetInactivityTimer();
